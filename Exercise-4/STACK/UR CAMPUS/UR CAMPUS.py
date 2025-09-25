b9e4cab60aa522ce66a5a85b5cfd6d09433fd170
MUKUNZI-JEAN-PAUL <mukunzijayp@gmail.com>
2025-09-25 16:59:09 +0200

    Add files via upload

---
+++ Exercise-4/STACK/UR CAMPUS/UR CAMPUS.py
@@ -0,0 +1,9 @@
+#UR CAMPUS 
+stack2 = ["QuizA", "QuizB", "QuizC"]
+print("Stack (bottom -> top):", stack2)
+
+p1 = stack2.pop()
+p2 = stack2.pop()
+print("Popped:", p1, p2)  #undo quiz C,quiz B
+print("Stack now (bottom -> top):", stack2)
+print("Top now:", stack2[-1])
